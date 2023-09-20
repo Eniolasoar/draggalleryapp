2efb5d0941b0b8ffbddf5c49170613729b141f09
Eniolasoar <eniolanishe@gmail.com>
2023-09-20 22:23:06 +0100

    image box styling

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,5 +1,11 @@
 import "./MainContent.css";
+import React, { useState } from 'react'; 
 function MainContent(){
+    const [heartColor, setBackgroundColor] = useState('#9CA3AF');
+
+  const handleClick = () => {
+    setBackgroundColor('red');
+  };
     return(
         <div className="main-content">
             <h1 className="galleryHeading">Welcome to Your Gallery</h1>
@@ -8,26 +14,151 @@ function MainContent(){
                 <button>Search</button>
             </div>
             <div className="imageContainer">
-                <img src="/home2.jpeg" alt="" width={300} height={300}/>
-                <img src="/home2.jpeg" alt="" width={300} height={300}/>
-                <img src="/home2.jpeg" alt="" width={300} height={300}/>
-                <img src="/home2.jpeg" alt="" width={300} height={300}/>
-                <img src="/home2.jpeg" alt="" width={300} height={300}/>
-                <img src="/home2.jpeg" alt="" width={300} height={300}/>
-                <img src="/home2.jpeg" alt="" width={300} height={300}/>
-                <img src="/home2.jpeg" alt="" width={300} height={300}/>
-                <img src="/home2.jpeg" alt="" width={300} height={300}/>
-                <img src="/home2.jpeg" alt="" width={300} height={300}/>
-                <img src="/home2.jpeg" alt="" width={300} height={300}/>
-                <img src="/home2.jpeg" alt="" width={300} height={300}/>
-                <img src="/home2.jpeg" alt="" width={300} height={300}/>
-                <img src="/home2.jpeg" alt="" width={300} height={300}/>
-                <img src="/home2.jpeg" alt="" width={300} height={300}/>
-                <img src="/home2.jpeg" alt="" width={300} height={300}/>
-                <img src="/home2.jpeg" alt="" width={300} height={300}/>
-                <img src="/home2.jpeg" alt="" width={300} height={300}/>
-                <img src="/home2.jpeg" alt="" width={300} height={300}/>
-                <img src="/home2.jpeg" alt="" width={300} height={300}/>
+                <div className="imageBox" style={{position:"relative"}}>
+                    <img src="/home2.jpeg" alt="" width={300} height={300}/>
+                    <img src="/heart.svg" alt="" className="heart" onClick={handleClick} style={{backgroundColor:heartColor}}/>
+                    <div className="imageTag">
+                        <p>Sport</p>
+                        <p>Diving</p>
+                    </div>
+                </div>
+
+                <div className="imageBox" style={{position:"relative"}}>
+                    <img src="/home2.jpeg" alt="" width={300} height={300}/>
+                    <img src="/heart.svg" alt="" className="heart" onClick={handleClick} style={{backgroundColor:heartColor}}/>
+                    <div className="imageTag">
+                        <p>Sport</p>
+                        <p>Diving</p>
+                    </div>
+                </div>
+
+                <div className="imageBox" style={{position:"relative"}}>
+                    <img src="/home2.jpeg" alt="" width={300} height={300}/>
+                    <img src="/heart.svg" alt="" className="heart" onClick={handleClick} style={{backgroundColor:heartColor}}/>
+                    <div className="imageTag">
+                        <p>Sport</p>
+                        <p>Diving</p>
+                    </div>
+                </div>
+
+                <div className="imageBox" style={{position:"relative"}}>
+                    <img src="/home2.jpeg" alt="" width={300} height={300}/>
+                    <img src="/heart.svg" alt="" className="heart" onClick={handleClick} style={{backgroundColor:heartColor}}/>
+                    <div className="imageTag">
+                        <p>Sport</p>
+                        <p>Diving</p>
+                    </div>
+                </div>
+
+                <div className="imageBox" style={{position:"relative"}}>
+                    <img src="/home2.jpeg" alt="" width={300} height={300}/>
+                    <img src="/heart.svg" alt="" className="heart" onClick={handleClick} style={{backgroundColor:heartColor}}/>
+                    <div className="imageTag">
+                        <p>Sport</p>
+                        <p>Diving</p>
+                    </div>
+                </div>
+
+                <div className="imageBox" style={{position:"relative"}}>
+                    <img src="/home2.jpeg" alt="" width={300} height={300}/>
+                    <img src="/heart.svg" alt="" className="heart" onClick={handleClick} style={{backgroundColor:heartColor}}/>
+                    <div className="imageTag">
+                        <p>Sport</p>
+                        <p>Diving</p>
+                    </div>
+                </div>
+
+                <div className="imageBox" style={{position:"relative"}}>
+                    <img src="/home2.jpeg" alt="" width={300} height={300}/>
+                    <img src="/heart.svg" alt="" className="heart" onClick={handleClick} style={{backgroundColor:heartColor}}/>
+                    <div className="imageTag">
+                        <p>Sport</p>
+                        <p>Diving</p>
+                    </div>
+                </div>
+
+                <div className="imageBox" style={{position:"relative"}}>
+                    <img src="/home2.jpeg" alt="" width={300} height={300}/>
+                    <img src="/heart.svg" alt="" className="heart" onClick={handleClick} style={{backgroundColor:heartColor}}/>
+                    <div className="imageTag">
+                        <p>Sport</p>
+                        <p>Diving</p>
+                    </div>
+                </div>
+
+                <div className="imageBox" style={{position:"relative"}}>
+                    <img src="/home2.jpeg" alt="" width={300} height={300}/>
+                    <img src="/heart.svg" alt="" className="heart" onClick={handleClick} style={{backgroundColor:heartColor}}/>
+                    <div className="imageTag">
+                        <p>Sport</p>
+                        <p>Diving</p>
+                    </div>
+                </div>
+
+                <div className="imageBox" style={{position:"relative"}}>
+                    <img src="/home2.jpeg" alt="" width={300} height={300}/>
+                    <img src="/heart.svg" alt="" className="heart" onClick={handleClick} style={{backgroundColor:heartColor}}/>
+                    <div className="imageTag">
+                        <p>Sport</p>
+                        <p>Diving</p>
+                    </div>
+                </div>
+
+                <div className="imageBox" style={{position:"relative"}}>
+                    <img src="/home2.jpeg" alt="" width={300} height={300}/>
+                    <img src="/heart.svg" alt="" className="heart" onClick={handleClick} style={{backgroundColor:heartColor}}/>
+                    <div className="imageTag">
+                        <p>Sport</p>
+                        <p>Diving</p>
+                    </div>
+                </div>
+
+                <div className="imageBox" style={{position:"relative"}}>
+                    <img src="/home2.jpeg" alt="" width={300} height={300}/>
+                    <img src="/heart.svg" alt="" className="heart" onClick={handleClick} style={{backgroundColor:heartColor}}/>
+                    <div className="imageTag">
+                        <p>Sport</p>
+                        <p>Diving</p>
+                    </div>
+                </div>
+
+                <div className="imageBox" style={{position:"relative"}}>
+                    <img src="/home2.jpeg" alt="" width={300} height={300}/>
+                    <img src="/heart.svg" alt="" className="heart" onClick={handleClick} style={{backgroundColor:heartColor}}/>
+                    <div className="imageTag">
+                        <p>Sport</p>
+                        <p>Diving</p>
+                    </div>
+                </div>
+
+                <div className="imageBox" style={{position:"relative"}}>
+                    <img src="/home2.jpeg" alt="" width={300} height={300}/>
+                    <img src="/heart.svg" alt="" className="heart" onClick={handleClick} style={{backgroundColor:heartColor}}/>
+                    <div className="imageTag">
+                        <p>Sport</p>
+                        <p>Diving</p>
+                    </div>
+                </div>
+
+                <div className="imageBox" style={{position:"relative"}}>
+                    <img src="/home2.jpeg" alt="" width={300} height={300}/>
+                    <img src="/heart.svg" alt="" className="heart" onClick={handleClick} style={{backgroundColor:heartColor}}/>
+                    <div className="imageTag">
+                        <p>Sport</p>
+                        <p>Diving</p>
+                    </div>
+                </div>
+
+                <div className="imageBox" style={{position:"relative"}}>
+                    <img src="/home2.jpeg" alt="" width={300} height={300}/>
+                    <img src="/heart.svg" alt="" className="heart" onClick={handleClick} style={{backgroundColor:heartColor}}/>
+                    <div className="imageTag">
+                        <p>Sport</p>
+                        <p>Diving</p>
+                    </div>
+                </div>
+                
+
             </div>
         </div>
 
